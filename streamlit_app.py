72e4fedc21232d8c78d43c98a460ddf83c3e8f9d
Radu-0 <117349096+Radu-0@users.noreply.github.com>
2024-05-20 13:02:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered! ' + smoothie_name, icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
